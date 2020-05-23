9bcb1032fcdbcd516aa65629cf88d03a8d8d8bb5
Joshua Blum <jblum18@gmail.com>
2020-05-23 12:09:42 -0400

    tweak

--- weather_cube.py
+++ weather_cube.py
@@ -9,13 +9,11 @@ from weather import reset_display, get_noaa_forecast, print_forecast, display_cu
 
 if __name__ == "__main__":
     reset_display()
-    forecast = get_noaa_forecast()
-    print_forecast(forecast)
     while True:
         try:
+            forecast = get_noaa_forecast()
             timeout = 60*5 if (forecast is None or not len(forecast.conditions)) else 60*60
             print 'Fetching new forecast'
-            forecast = get_noaa_forecast()
             print_forecast(forecast)
             display_cube(forecast)
             time.sleep(timeout)
